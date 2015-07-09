fd6355188988f66976e1643de513158cfe3230b4
UBUNTU <ubuntu@ubuntu>
2015-07-09 18:27:45 +0200

    toolbar done, working on tabs

--- app/src/app.js
+++ app/src/app.js
@@ -1,3 +1,16 @@
-angular.module('toolbarDemo1', ['ngMaterial'])
-  .controller('AppCtrl', function($scope) {
+var app = angular.module('toolbarDemo1', ['ngMaterial'])
+  app.controller('AppCtrl', function($scope) {
+
   });
+
+app.controller("tabsCtrl",function($scope)
+{
+  var tabData = {
+  "Inspiracie",
+  "Produkty",
+  "Clanky",
+  "Specialisti",
+  "Forum"
+};
+
+});
