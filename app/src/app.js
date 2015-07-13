52936ce6f83036864f4883d0fbbc98ed7de164cd
Michal Krajnak <michaall.k@gmail.com>
2015-07-13 21:02:38 +0200

    md tab body updated

--- app/src/app.js
+++ app/src/app.js
@@ -10,6 +10,29 @@ var app = angular.module('forumDizajn', ['ngMaterial'])
 
 app.controller("tabsCtrl",function($scope)
 {
-  this.tabsData = ["Inspiracie","Produkty","Clanky","Specialisti","Forum"];
+  this.tabsData =
+      [
+        {
+          name: "Inspiracie",
+          description: "Inspirujte sa niecim novym"
+        },
+        {
+          name: "Produkty",
+          description: "Pozrite si nase produkty"
+        },
+        {
+          name: "Clanky",
+          description: "Sledujte najnovsie clanky"
+        },
+        {
+          name: "Specialisti",
+          description: "Nechajte sa poradit nasimi specialistami"
+        },
+        {
+          name: "Forum",
+          description: "Pozrite si najcitanejsie forum"
+        }
+      ]
+
 
 });
