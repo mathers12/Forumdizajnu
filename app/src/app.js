66a24254e39f6cd8e330ba35e425e5d90b730c5e
Michal Krajnak <michaall.k@gmail.com>
2015-07-15 15:45:59 +0200

    sideNav added

--- app/src/app.js
+++ app/src/app.js
@@ -1,12 +1,46 @@
-var app = angular.module('forumDizajn', ['ngMaterial'])
-  .config(function($mdThemingProvider) {
+var app = angular.module('forumDizajnu', ['ngMaterial','ui.router'])
+  .config(function($mdThemingProvider,$locationProvider,$urlRouterProvider) {
     $mdThemingProvider.theme('themeProvider')
       .primaryPalette('green')
       .accentPalette('orange');
-  });
-  app.controller('toolbarCtrl', function($scope) {
+
+
 
   });
+
+
+  app.controller('toolbarCtrl', function($scope,$timeout, $mdSidenav, $mdUtil, $log) {
+
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+    /**
+     * Build handler to open/close a SideNav; when animation finishes
+     * report completion in console
+     */
+    function buildToggler(navID) {
+      var debounceFn =  $mdUtil.debounce(function(){
+        $mdSidenav(navID)
+          .toggle()
+          .then(function () {
+            $log.debug("toggle " + navID + " is done");
+          });
+      },300);
+      return debounceFn;
+    }
+
+    $scope.close = function () {
+      $mdSidenav('left').close()
+        .then(function () {
+          $log.debug("close LEFT is done");
+        });
+    };
+
+    $scope.categoryClick = function()
+    {
+
+    }
+  });
+
 
 app.controller("tabsCtrl",function($scope)
 {
